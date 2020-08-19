c627414dfcd77295f7fa92dc6556aaba37a082c6
Serah Sirohi <serahsirohi@gmail.com>
2020-08-19 22:04:16 +0000

    INSERTed new data

---
+++ migrations/tempfile.js
@@ -0,0 +1,12 @@
+const merge = function(array1, array2) {
+
+    let arrayJoin = [];
+
+    arrayJoin = array1.concat(array2);
+
+    return arrayJoin.sort();
+}
+
+console.log(merge([4, 5, 6], [1, 2, 3, 4]), "=?", [1, 2, 3, 4, 4, 5, 6]);
+console.log(merge([4], [2, 5, 8]), "=?", [2, 4, 5, 8]);
+console.log(merge([1, 2, 6], []), "=?", [1, 2, 6]);
